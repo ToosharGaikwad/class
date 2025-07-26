fa0330373d67a6b7f2a19d704bfde479428f640b
Tushar <142976144+ToosharGaikwad@users.noreply.github.com>
2025-07-26 18:41:39 +0530

    Add files via upload

---
+++ c program/Dicision making/evenOdd.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include <ctype.h>
+void main(){
+//	int no = 10;
+//	if(no>0 ){
+
+//		printf("no is positive");
+//	}else{
+//		printf("no is negative");
+//	}
+
+//int year=2000;
+//
+//if(year%4==0 && year%100!=0 || year%400==0) 
+//	printf("year is leap");
+//
+//else
+//	printf("year is not leap" );
+
+
+
+// scanf practice
+//int a,b;
+//printf("enter a number");
+//scanf("%d %d,&a &b");
+
+//if(a>b)
+//printf("a is greater then b");	
+//else
+//    printf("b is greater then a");
+
+//int age ;
+//printf("enter the age");
+//scanf("%d",&age );
+//
+//
+//if(age >=18 && age<100)
+//    printf("You are egible for voting");
+//else
+//    ("You are not egible for voting");
+
+
+
+// Assingment 
+// number is pelindrome or not 
+//int no;
+//printf("enter a number");
+//scanf("%d",&no);
+//
+// if (no < 100 || no > 999) 
+//        printf("Not a 3-digit number.\n");
+//        return 0;
+//
+//int temp = no/100;
+//int temp2 = no%10;
+//
+//if(temp==temp2){
+//	printf("number is pelindrome");
+//}
+//else
+//    printf("number is not pelindrome");
+
+
+//check vovel or consonents
+
+//char vowel;
+//
+//printf("enter the character  ");
+//
+//scanf("%c",&vowel);
+//
+//if(vowel == 'a'|| vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u'){
+//	
+//	printf("the character is vowel");
+//}else
+//printf("the character is consonant.");
+
+
+// uppercase or lowercase
+
+char ch ;
+printf("enter a character   ");
+  scanf("%c",&ch);
+  
+if(isupper(ch))
+   printf("character is uppercase");
+else if(islower(ch))
+    printf("character is lowercase");
+    else
+    printf("not an alphabate");
+
+
+
+
+
+
+
+
+
+
+}
