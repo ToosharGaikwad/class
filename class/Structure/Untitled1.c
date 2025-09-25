8edb2b5c803987e55ef13a223b0f922442c8251e
Tushar <gaikwadtushar@gmail.com>
2025-09-25 15:20:30 +0530

    arrary prime number

---
+++ class/Structure/Untitled1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main(){
+int arr[] = {1,2,3,4,5,6,8,9,10,11,13,15};
+
+int  size = sizeof(arr)/sizeof(int);
+
+
+for(int i=0;i<size;i++){
+	int flag = 0;
+	if(arr[i]!=1){
+		
+		
+	}
+for(int j=2;j<size/2;j++){
+		if(arr[i]%j==0){
+		flag =1;
+		break;
+		
+	}	
+}
+     if(flag==0){
+		printf("number is Prime %d" ,arr[i]);
+	}
+	
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+}
