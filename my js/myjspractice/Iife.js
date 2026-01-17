05daf0bd3db76a1d3370a699accd3ea5a68721bf
Tushar <gaikwadtushar@gmail.com>
2026-01-17 08:18:43 +0530

    add my file  practice problems

---
+++ my js/myjspractice/Iife.js
@@ -0,0 +1,13 @@
+// (function chai() {
+//   console.log(`db connect `);
+// })(); /// named iife
+
+// ((username) => {
+//   console.log(`db connecte to ${username} `);
+// })("tushar");
+
+const coding = ["js", "python", "java", "html", "springs"];
+const val = coding.forEach((item) => {
+  console.log(item);
+});
+console.log(val);
